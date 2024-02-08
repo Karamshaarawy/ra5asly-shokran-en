955ead09199fc2b4bdb0341c66ba96b09a836f88
Karam Shaarawy <karam.shaarawy@shelter-technology.com>
2024-02-09 00:49:28 +0200

    Refactor login page background styling
    Added consistent background styling to improve visual appeal and maintain design coherence. No functional changes.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -38,7 +38,7 @@ export default function LoginPage() {
 
   return (
     <div>
-      <div className="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 h-screen">
+      <div className="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 h-screen bg-white">
         <div className="md:flex md:items-center md:justify-end w-full bg-white sm:w-auto md:h-full md:w-1/3 py-8 px-4 sm:p-12 md:p-16 sm:rounded-2xl md:rounded-none sm:shadow md:shadow-none sm:bg-card">
           <div className="w-full max-w-80 sm:w-80 mx-auto sm:mx-0">
             <div className="w-auto">
@@ -112,7 +112,7 @@ export default function LoginPage() {
             </Form>
           </div>
         </div>
-        <div className="relative hidden md:flex flex-auto items-center justify-center w-1/4 h-full p-16 lg:px-28 overflow-hidden bg-[url('/images/log-in-bg.png')] dark:border-l"></div>
+        <div className="relative hidden md:flex flex-auto items-center justify-center w-1/4 h-full p-16 lg:px-28 overflow-hidden bg-no-repeat bg-center  bg-contain bg-[url('/images/log-in-bg.png')] dark:border-l"></div>
       </div>
     </div>
   );
