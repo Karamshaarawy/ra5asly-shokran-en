029887ce7dbc1707d2a55cf852267e198000ef26
Karam Shaarawy <karam.shaarawy@shelter-technology.com>
2024-02-10 19:27:42 +0200

    govs listing

--- app/dashboard/govs/page.tsx
+++ app/dashboard/govs/page.tsx
@@ -347,7 +347,7 @@ export default function Govs() {
       <div>
         <div className="w-full max-h-screen overflow-x-scroll lg:overflow-x-auto md:overflow-x-scroll sm:overflow-x-scroll">
           <Table
-            pagination={{ position: ["none", "none"] }}
+            // pagination={{ position: ["none", "none"] }}
             scroll={{ x: 0 }}
             rowKey={"id"}
             columns={columns}
@@ -355,7 +355,7 @@ export default function Govs() {
             expandable={{
               expandedRowRender: (record) => (
                 <Table
-                  pagination={{ position: ["none", "none"] }}
+                  // pagination={{ position: ["none", "none"] }}
                   className=" p-5"
                   scroll={{ x: 0 }}
                   rowKey={"id"}
